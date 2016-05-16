911bf4d9fb9563afde3c7d49fead5c53b6328772
Boucar Sagne <tiagoboucar@yahoo.fr>
2016-05-16 19:28:33 +0000

    update copyright

--- symfony/apps/orangehrm/templates/_copyright.php
+++ symfony/apps/orangehrm/templates/_copyright.php
@@ -1,8 +1,8 @@
 <?php 
-$version = '3.3.2';
-$prodName = 'OrangeHRM';
+//$version = '3.3.2';
+$prodName = 'SEPRO ERP';
 $copyrightYear = date('Y');
 
 ?>
-<?php echo $prodName . ' ' . $version;?><br/>
-&copy; 2005 - <?php echo $copyrightYear;?> <a href="http://www.orangehrm.com" target="_blank">OrangeHRM, Inc</a>. All rights reserved.
+<?php echo $prodName;?><br/>
+&copy; 2005 - <?php echo $copyrightYear;?> <a href="http://www.sepro.com" target="_blank">Sepro ERP</a>.
